44421d8c58db6978aec59c7fb1e9b084c22fa2c2
lujainabusada <lujein.abusada@gmail.com>
2022-07-29 12:21:41 +0300

    add validation script that validates if httpd is installed on client container or not

--- Server/runContainer.sh
+++ Server/runContainer.sh
@@ -18,4 +18,4 @@ fi
 
 docker build -t lujainabusada/apacheserver  /home/lujainabusada/ApacheServerClient/Server
 docker run -d -it -p 8899:80 --name httpd-container lujainabusada/apacheserver  
-
+docker cp httpd-container://localrepo /
---
+++ Validation/testhttpd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+httpd=$(docker exec -ti httpd-client sh -c "rpm -qa"| grep httpd | head -1)
+
+if [ $httpd ]
+then
+echo "httpd is installed on client container"
+else
+echo "httpd is not installed on client container"
+fi
+
